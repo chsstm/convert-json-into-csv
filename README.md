9b7b71d36c571f6db315f21d9feb8ba6572477bc
Saimon Thada Magar <magarchsstm@gmail.com>
2018-04-18 18:50:53 +0530

    First push

---
+++ README.md
@@ -0,0 +1,35 @@
+### Convert JSON into CSV
+
+It is a npm package with a function which receives a `JSON` object as a parameter and returns a `CSV` data to show in tabular form.
+
+### Setup process
+1. Run **npm install convert-json-into-csv** 
+
+### Use of method
+
+#### Code
+```javascript
+import JsonToCsv from 'convert-json-into-csv'; //ES6
+//OR
+const JsonToCsv = require('convert-json-into-csv'); //ES5
+
+let csv = ``;
+let json = {
+    name: 'Saimon Thada Magar',
+    company: 'ChicMic, Mohali, India',
+    profession: 'Software Engineer'
+}
+
+csv = JsonToCsv(json);
+console.log(csv);
+```
+
+#### Output (return value):
+```
+"name","company","profession"
+"Saimon Thada Magar","ChicMic, Mohali, India","Software Engineer"
+```
+
+### Upcoming new Features
+1. User can select symbol for text **delimiter**.
+2. User can select symbol for text **separation**.
---
+++ index.js
@@ -0,0 +1,104 @@
+export default JSONintoCSV = (json) => {
+    var plainData = {};
+    var csv = ``;
+
+    plainData = parseInner(json,plainData);
+
+    let rows = getRowLength(plainData);
+    let cols = Object.keys(plainData).length;
+
+    let count = 0;
+    //Writing column header with keys
+    for(let key in plainData){
+        csv += '"'+key+'"'+ (count<cols-1?',':'\n');
+        count++;
+    }
+
+    //Writing row data with values
+    for(let row=0; row<rows; row++){
+        let count = 0;
+        for(let key in plainData){
+            if(plainData[key][row]){
+                csv += '"'+plainData[key][row]+'"';
+            }else{
+                csv += '""';
+            }
+            csv += count<cols-1?',':'\n';
+            count++;
+        }
+    }
+
+    return csv;
+}
+
+//Get rows length
+function getRowLength(plainData){
+    for(let key in plainData){
+        return plainData[key].length;
+    }
+}
+
+function parseInner(json,plainData,parentKey){
+    let newData = plainData;
+    for(let key in json){
+        if(typeof(json[key]) === 'object'){
+            let newKey = parentKey? parentKey+"_"+key:key;
+            if(!parentKey && Object.keys(json).length === 1){
+                newKey = null;
+            }
+            if(json[key].length > 0){
+                if(typeof(json[key][0]) === 'object'){
+                    for(let arrayObj of json[key]){
+                        newData = parseInner(arrayObj,newData,newKey);
+                    }
+                }else{
+                    for(let value of json[key]){
+                        if(newData.hasOwnProperty(newKey) ===  true){
+                            newData[newKey].push(value);
+                        }else{
+                            newData[newKey] = [];
+                            newData[newKey].push(value);
+                        }
+
+                        let newLength = newData[newKey].length;
+                        for(let objKey in newData){
+                            if(newData[objKey].length === newLength){
+                                break;
+                            }
+                            while(newData[objKey].length < newLength){
+                                newData[objKey].push(null);
+                            }
+                        }
+                    }
+                }
+            }else{
+                newData = parseInner(json[key],newData,newKey);
+            }
+        }else{
+            let value = json[key];
+            let newKey = parentKey? parentKey+"_"+key:key;
+            if(newData.hasOwnProperty(newKey) ===  true){
+                newData[newKey].push(value);
+            }else{
+                newData[newKey] = [];
+                let rows = getRowLength(plainData);
+                while(rows-1 > 0){
+                    newData[newKey].push(null);
+                    rows--;
+                }
+                newData[newKey].push(value);
+            }
+
+            let newLength = newData[newKey].length;
+            for(let objKey in newData){
+                if(newData[objKey].length === newLength){
+                    break;
+                }
+                while(newData[objKey].length < newLength){
+                    newData[objKey].push(null);
+                }
+            }
+        }
+    }
+    return plainData;
+}
